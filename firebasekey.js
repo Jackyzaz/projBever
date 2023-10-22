fa98aa7cbe54435353f0f1f7ab0e0eebbfd8b845
Soravit Sukkarn <64414631+Jackyzaz@users.noreply.github.com>
2023-10-22 21:35:34 +0700

    Delete firebasekey.js

--- firebasekey.js
+++
@@ -1,7 +0,0 @@
-export const REACT_APP_FIREBASE_APIKEY = "AIzaSyCdDNMHCQlHS0B_sLe6MEuNRIyirIbfgfk"
-export const REACT_APP_FIREBASE_AUTHDOMAIN = "projbever.firebaseapp.com"
-export const REACT_APP_FIREBASE_PROJECTID = "projbever"
-export const REACT_APP_FIREBASE_STORAGEBUCKET = "projbever.appspot.com"
-export const REACT_APP_FIREBASE_APPID = "1:61471535275:web:32c31b129b1940bf0b07ee"
-export const REACT_APP_FIREBASE_MESSAGINGSENDERID = "61471535275"
-export const REACT_APP_FIREBASE_MEASUREMENTID = "G-KWREWSMC6J"
